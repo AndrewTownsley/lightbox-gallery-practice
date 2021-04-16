88a6c0c075234a9705aa3e16c033ed0a1d83fed0
Andrew TOwnsley <atownsley86@gmail.com>
2021-04-16 00:50:06 -0600

    Gallery layout complete, modal created and is displayed on image click

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
+// const lightbox = document.createElement('div');
+// lightbox.id = 'lightbox';
+// document.body.appendChild(lightbox);
+const lightbox = document.getElementById('lightbox');
 
 const img = document.querySelectorAll('#img');
 console.log(img);
 img.forEach(image => {
   image.addEventListener('click', e => {
     lightbox.classList.add('active');
+    const modalImg = document.createElement('img');
+    modalImg.classList.add('modal-img');
+    modalImg.src = image.src;
+    lightbox.appendChild(modalImg);
+    
+    while(lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(modalImg);
   })
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.classList.remove('active');
 })
